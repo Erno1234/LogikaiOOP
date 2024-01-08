61d10b3ab7c0aff6650e2fea38daa75448c6526d
sw4q3ar2 <biggercsgo@gmail.com>
2024-01-08 13:54:31 +0100

    scanner

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -2,17 +2,19 @@
 package logikaioop;
 
 import java.util.Scanner;
-
+    
 public class LogikaiOOP {
-
+    
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         System.out.println("Leírás");
         System.out.println("Az 1. állitás!");
         System.out.println("A 2. állitás!");
         System.out.println("A 3. állitás!");
         System.out.println("Melyik:");
+        String beker = sc.nextLine();
         System.out.println("A válasz jó | rossz");
-        Scanner sc = new Scanner(System.in);
     }
     
 }
