626282fa65ae5d2ab841922e875dd7eca1da4556
sw4q3ar2 <biggercsgo@gmail.com>
2024-01-08 15:43:50 +0100

    Eredmeny

---
+++ src/logikaioop/Kontroller.java
@@ -0,0 +1,14 @@
+
+package logikaioop;
+
+public class Kontroller {
+    
+    private void eredemeny(int valasz) {
+        if(valasz == 2){
+            System.out.println("A válasz jó");
+        }
+        else{
+            System.out.println("A válasz rossz");
+        }
+    }
+}
--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -2,15 +2,7 @@ package logikaioop;
 
 public class LogikaiOOP {
     public static void main(String[] args) {
-        Allitas allitas1 = new Allitas("1. Állitás!");
-        Allitas allitas2 = new Allitas("2. Állitás!");
-        Allitas allitas3 = new Allitas("3. Állitás!");
-        System.out.println("Leírás");
-        System.out.println(allitas1);
-        System.out.println(allitas2);
-        System.out.println(allitas3);
-        System.out.println("Melyik:");
-        System.out.println("A válasz jó | rossz");
+        new Kontroller();
     }
 
 }
