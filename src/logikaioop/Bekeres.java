27509381371f7562afdcabd46403c9fca5267b4f
Martonnvok <martonsom@gmail.com>
2024-01-08 14:01:50 +0100

    Bekeres osztály létrehozása

---
+++ src/logikaioop/Bekeres.java
@@ -0,0 +1,49 @@
+
+package logikaioop;
+
+
+public class Bekeres {
+    private int allitas;
+
+    public Bekeres(int allitas) {
+        this.allitas = allitas;
+    }
+    
+    public int getAllitas() {
+        return allitas;
+    }
+
+    public void setAllitas(int allitas) {
+        this.allitas = allitas;
+    }
+
+    @Override
+    public String toString() {
+        return "Az " + allitas +". állítás!";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.allitas;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bekeres other = (Bekeres) obj;
+        return this.allitas == other.allitas;
+    }
+    
+    
+    
+}
--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -8,10 +8,13 @@ public class LogikaiOOP {
     static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
+        Bekeres b1 = new Bekeres(1);
+        Bekeres b2 = new Bekeres(2);
+        Bekeres b3 = new Bekeres(3);
         System.out.println("Leírás");
-        System.out.println("Az 1. állitás!");
-        System.out.println("A 2. állitás!");
-        System.out.println("A 3. állitás!");
+        System.out.println(b1.toString());
+        System.out.println(b2.toString());
+        System.out.println(b3.toString());
         System.out.println("Melyik:");
         String beker = sc.nextLine();
         System.out.println("A válasz jó | rossz");
