a2f5838813b9e921ec0fac6aadec2651497da8c6
Martonnvok <martonsom@gmail.com>
2024-01-08 14:07:44 +0100

    Helyesírási hibák javítása

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -10,9 +10,9 @@ public class LogikaiOOP {
         Bekeres b2 = new Bekeres(2);
         Bekeres b3 = new Bekeres(3);
         System.out.println("Leírás");
-        System.out.println("Az"+b1.toString()+". állítás!");
-        System.out.println("Az"+b2.toString()+". állítás!");
-        System.out.println("Az"+b3.toString()+". állítás!");
+        System.out.println("Az "+b1.toString()+". állítás!");
+        System.out.println("A "+b2.toString()+". állítás!");
+        System.out.println("A "+b3.toString()+". állítás!");
         System.out.println("Melyik:");
         System.out.println("A válasz jó | rossz");
     }
